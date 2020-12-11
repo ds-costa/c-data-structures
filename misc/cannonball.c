4215db18d19ac0b715f5808e40f2328de8f69613
ds-costa <davidshcosta@gmail.com>
2020-12-11 10:17:57 -0300

    refactoring, /misc - tab 4

---
+++ misc/cannonball.c
@@ -0,0 +1,6 @@
+int cannonBallPyramid(int layers) {
+	if(layers == 1) {
+		return 1;
+	}
+	return (layers * layers) + cannonBallPyramid(layers - 1);
+}
---
+++ misc/cel2fahr.c
@@ -0,0 +1,7 @@
+double celToFahr(double cel) {
+	return cel * (9 / 5) + 32;
+}
+
+double fahrToCel(double fahr) {
+	return (fahr - 32) * 5 / 9;
+}
