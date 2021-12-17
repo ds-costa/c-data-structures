966a70ddd5aa2a46e6cb5c1aca711bfa59097f9b
ds-costa <davidshcosta@gmail.com>
2021-12-17 13:41:54 -0300

    update(array): shl file

--- main.c
+++ main.c
@@ -1,8 +1,12 @@
 #include <stdio.h>
+// #include "src/array.h"
+// #include "src/strings.h"
+// #include "src/linkedList.h"
+// #include "src/sort.h"
+
+#define ARRAY_IMPLEMENTATION_H
 #include "src/array.h"
-#include "src/strings.h"
-#include "src/linkedList.h"
-#include "src/sort.h"
+
 
 int main() {
 
@@ -15,12 +19,10 @@ int main() {
     };
     
     double arr3[] = {
-        123.333,76, 0, 3, 10, 434, 22, -1, 4, 99, 1, 12408, -12
+        122.333,76, 0, 3, 10, 434, 22, -1, 4, 99, 1, 12408, -12
     };
 
-    arr_print_int(12, arr);
-    arr_print_float(12, arr2);
-    arr_print_double(12, arr3);
+    arr_print(12, arr);
 
     return 0;
 }
--- src/array.h
+++ src/array.h
@@ -1,90 +1,140 @@
 #ifndef ARRAY_H
 #define ARRAY_H
 
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+
 #define ARRAY_SIZE(array) (sizeof(array) / sizeof(array[0]))
 
 /**
- * function: arr_scan_int
- * Writes to the array the (int) content read in the stdin
- * @param arr_len, Array length
- * @param array, Integer array reference
+ * @brief Reads the contents of stdin and writes to the array 
+ * 
+ * @param arr_len 
+ * @param array 
  */
-void arr_scan_int( int arr_len, int *array );
+void arr_scan( int arr_len, int *array );
 
 /**
- * function: arr_print_int
- * Shows the contents of the (int) array in stdout
- * @param arr_len, Array length
- * @param array, Integer array reference
+ * @brief Reads the contents of the array and writes to stdout
+ * 
+ * @param arr_len 
+ * @param array 
  */
-void arr_print_int( int arr_len, int *array );
+void arr_print( int arr_len, int *array );
 
 /**
- * function: arr_scan_float
- * Writes to the array the (float) content read in the stdin
- * @param arr_len, Array length
- * @param array, Integer array reference
+ * @brief Fills an array with random numbers in the inclusive range [high, low]
+ * 
+ * @param low 
+ * @param high 
+ * @param arr_len 
+ * @param arr 
  */
-void arr_scan_float( int arr_len, float *array );
+void arr_random_fill(int low, int high, int arr_len, int *arr);
 
 /**
- * function: arr_print_float
- * Shows the contents of the (float) array in stdout
- * @param arr_len, Array length
- * @param array, Integer array reference
+ * @brief Shuffle an array. Original algo By: Richard Durstenfeld (1964) AND Ronald Fisher and Frank Yates (1938) 
+ * 
+ * @param arr_len 
+ * @param arr 
  */
-void arr_print_float( int arr_len, float *array );
+void arr_shuffle(int arr_len, int *arr);
+
 
 /**
- * function: arr_scan_double
- * Writes to the array the (double) content read in the stdin
- * @param arr_len, Array length
- * @param array, Integer array reference
+ * @brief Linear Search Algorithm implementation 
+ * 
+ * @param key 
+ * @param arr_len 
+ * @param arr 
+ * @return int 
  */
-void arr_scan_double( int arr_len, double *array );
+int arr_linear_search(int key, int arr_len, int *arr);
 
 /**
- * function: arr_print_double
- * Shows the contents of the (double) array in stdout
- * @param arr_len, Array length
- * @param array, Integer array reference
+ * @brief Binary Search Algorithm implementation for sorted arrays
+ * 
+ * @param key 
+ * @param arr_len 
+ * @param arr 
+ * @return int 
  */
-void arr_print_double( int arr_len, double *array );
+int arr_binary_search(int key, int arr_len, int *arr);
 
-/**
- * function: arr_random_fill_int
- * Fill an integer array with random numbers in an interval [low, high]
- * @param low, inferior limit 
- * @param high, superior limit
- * @param arr_len, Array length
- * @param arr, Integer array reference
- */
-void arr_random_fill_int(int low, int high, int arr_len, int *arr);
 
-/**
- * function: arr_shuffle_int
- * Shuffle an array.  Original algo By: Richard Durstenfeld (1964) AND Ronald Fisher and Frank Yates (1938) 
- * @param arr_len, Array length
- * @param arr, Integer array reference
- */
-void arr_shuffle_int(int arr_len, int *arr);
+#ifdef ARRAY_IMPLEMENTATION_H
 
-/**
- * function: arr_linear_search_int
- * Implementation of linear search algorithm
- * @param key, Target value
- * @param arr_len, Array length
- * @param arr, Integer array reference
- */
-int arr_linear_search_int(int key, int arr_len, int *arr);
+void arr_scan(int arr_len, int *array) {
+    int i;
+    for(i = 0; i < arr_len; i++) {
+        scanf("%d", &array[i]);
+    }
+}
 
-/**
- * function: arr_binary_search_int
- * Implementation of binary search algorithm for sorted arrays
- * @param key, Target value
- * @param arr_len, Array length
- * @param arr, Target integer array reference
- */
-int arr_binary_search_int(int key, int arr_len, int *arr);
+void arr_print(int arr_len, int *array) {
+	int i;
+	printf("[ ");
+    for(i = 0; i < arr_len; i++) {
+		printf("%d, ", array[i]);
+	}    
+	printf("]\n");
+}
 
+void arr_random_fill(int low, int high, int arr_len, int *arr) 
+{
+    int i;
+    for(i = 0; i < arr_len; ++i) {
+        arr[i] = ( low + rand() % ( high - low + 1 ));
+    }
+}
+
+void arr_shuffle(int arr_len, int *arr) {
+    int i, j, aux;
+	for(i = 0 ; i < arr_len ; i++) {
+		j = (rand() % arr_len);
+		aux = arr[i];
+		arr[i] = arr[j];
+		arr[j] = aux;
+	}
+}
+
+int arr_linear_search(int key, int arr_len, int *arr) {
+    int i;
+    for(i = 0; i < arr_len; i++) {
+        if(arr[i] == key) {
+            return i;
+        }
+    }
+    return -1; 
+}
+
+int arr_binary_search(int key, int arr_len, int *arr) {
+    int low = 0;
+    int high = arr_len;
+    int mid;
+
+    while(low < high) {
+    
+        mid = (high + low) / 2;
+        
+        if(arr[mid] == key) {
+            return mid;
+        }
+        
+        if(arr[mid] < key) {
+            low = mid;
+        }
+
+        else if(arr[mid] > key) {
+            high = mid;
+        }
+    }   
+
+    return -1;
+}
+
+#endif
 #endif
