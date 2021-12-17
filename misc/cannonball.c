2940a32f960dd781bfa3bc6d37d0bf3a7dddfc0e
ds-costa <davidshcosta@gmail.com>
2021-12-17 12:24:45 -0300

    removing misc code

--- misc/cannonball.c
+++
@@ -1,6 +0,0 @@
-int cannonBallPyramid(int layers) {
-	if(layers == 1) {
-		return 1;
-	}
-	return (layers * layers) + cannonBallPyramid(layers - 1);
-}
--- misc/cel2fahr.c
+++
@@ -1,7 +0,0 @@
-double celToFahr(double cel) {
-	return cel * (9 / 5) + 32;
-}
-
-double fahrToCel(double fahr) {
-	return (fahr - 32) * 5 / 9;
-}
